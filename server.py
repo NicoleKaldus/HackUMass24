86f24fc4ec4af31d61ce804f0625c64540934523
massbchou <70417919+massbchou@users.noreply.github.com>
2024-11-09 12:39:51 -0500

    Basic attempt

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, send_from_directory
+import random
+
+app = Flask(__name__)
+
+#svelte path
+@app.route("/")
+def base():
+    return send_from_directory("/client/public", "index.html")
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+
+#test function, returns a random number
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 100))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
